37f88f9cb010867809e10bde4e99e2abb958cb10
Vignesh-737 <5737vignesh@gmail.com>
2025-11-23 21:48:24 +0530

    edit

--- Server/server.js
+++ Server/server.js
@@ -32,7 +32,7 @@ app.post("/chat", async (req, res) => {
   }
 });
 
-const PORT=process.env.port || 5000;
+const PORT=process.env.PORT || 5000;
 
 app.listen(PORT,()=>{
     console.log("Server running on port",PORT);
